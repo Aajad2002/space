224e2562a9fd0a592df1b86b6916c363872e406d
Aajad2002 <aajadprajapat28@gmail.com>
2023-09-03 12:43:29 +0530

    space-user-authentication

---
+++ routes/User.Route.js
@@ -0,0 +1,56 @@
+
+const express = require('express');
+require('dotenv').config()
+const userRouter = express.Router()
+
+//user model
+const UserModel = require("../models/User.Model")
+
+//bcrypt for hashing the password
+const bcrypt = require('bcrypt');
+
+//Registration
+userRouter.post("/register", async (req, res) => {
+    try {
+        let { email, password } = req.body
+        let user = await UserModel.findOne({ email })
+
+        if (user) {
+            res.status(200).send({ "msg": "User already registered on this email,Login now!!" })
+        } else {
+            bcrypt.hash(password, 6, async function (err, hash) {
+                const user = new UserModel({ ...req.body, password: hash })
+                await user.save()
+                res.status(200).send({ "msg": "Registration Successfull,Login Now!!" })
+            });
+        }
+    } catch (error) {
+        console.log("unable to register the new user")
+        res.status(400).send({ "err": error.message })
+    }
+})
+//login 
+userRouter.post('/login', async (req, res) => {
+    try {
+        let { email, password } = req.body
+        let user = await UserModel.findOne({ email })
+        if (user) {
+            bcrypt.compare(password, user.password, function (err, result) {
+                if (result) {
+
+                    res.status(200).send({ "msg": "Login Successfull", name: user.name, status: true })
+                } else {
+                    res.status(200).send({ "msg": "Wrong Password !!", status: false })
+                }
+            })
+        } else {
+            res.status(200).send({ "msg": "Email doesn't exist,Signup First!!" })
+        }
+    } catch (error) {
+        console.log("unable to Login the new user")
+        res.status(400).send({ "err": error.message })
+    }
+
+})
+
+module.exports = userRouter
